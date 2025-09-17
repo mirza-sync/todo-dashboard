eaba8c2249ed6fb45515cd64596c6ec21be352ad
mirza-sync <mirzasyahmi123@gmail.com>
2025-09-17 16:55:03 +0800

    Add navbar and layout styling

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,75 @@
+import { styled } from "styled-components";
+import { useAuth } from "../hooks/useAuth";
+import { useNavigate } from "react-router";
+import Button from "./ui/Button";
+
+type NavbarProps = {};
+
+const Nav = styled.nav`
+  height: 72px;
+  background: #ffffff;
+  box-shadow: 0px 3px 6px #00000029;
+  margin-bottom: 12px;
+  width: 100%;
+
+  @media (min-width: 769px) {
+    margin-bottom: 22px;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  margin-inline: 24px;
+
+  @media (min-width: 769px) {
+    max-width: 960px;
+    margin-inline: auto;
+  }
+`;
+
+const Profile = styled.nav`
+  display: flex;
+  align-items: center;
+`;
+
+const Avatar = styled.div`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  background: #e8ecec;
+  margin-right: 16px;
+`;
+
+const Text = styled.span`
+  font: normal normal 500 16px/19px Montserrat;
+  color: #6d8187;
+  text-transform: capitalize;
+`;
+
+const Navbar = () => {
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/login");
+  };
+
+  return (
+    <Nav>
+      <Wrapper>
+        <Profile>
+          <Avatar />
+          <Text>{user?.username}</Text>
+        </Profile>
+        <Button widthFull={false} ghost onClick={() => handleLogout()}>
+          <Text>Logout</Text>
+        </Button>
+      </Wrapper>
+    </Nav>
+  );
+};
+
+export default Navbar;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,3 @@
-import { useNavigate } from "react-router";
-import { useAuth } from "../hooks/useAuth";
 import NoTask from "../components/NoTask";
 import { useTask } from "../hooks/useTask";
 import TaskList from "../components/TaskList";
@@ -7,9 +5,22 @@ import CompletedTasks from "../components/CompletedTasks";
 import LatestTasks from "../components/LatestTasks";
 import TaskChart from "../components/TaskChart";
 import InfoWrapper from "../components/ui/InfoWrapper";
+import Navbar from "../components/Navbar";
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 100%;
+  background: #f4f4f6;
+`;
+
+const Layout = styled.div`
+  @media (min-width: 769px) {
+    max-width: 960px;
+    margin-inline: auto;
+  }
+`;
 
 const Dashboard = () => {
-  const { user, logout } = useAuth();
   const {
     allTasks,
     completedTask,
@@ -19,50 +30,40 @@ const Dashboard = () => {
     deleteTask,
     toggleTask,
   } = useTask();
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    logout();
-    navigate("/login");
-  };
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div
-      style={{
-        height: "100%",
-        background: "#F4F4F6 0% 0% no-repeat padding-box",
-      }}
-    >
-      <nav>
-        <div>Avatar</div>
-        <div>{user?.username}</div>
-        <button onClick={() => handleLogout()}>Logout</button>
-      </nav>
-      {allTasks.length === 0 ? (
-        <NoTask />
-      ) : (
-        <>
-          <InfoWrapper>
-            <CompletedTasks allTasks={allTasks} completedTask={completedTask} />
-            <LatestTasks allTasks={latestTask} />
-            <TaskChart
-              taskCount={allTasks.length}
-              completedCount={completedTask.length}
+    <Container>
+      <Navbar />
+      <Layout>
+        {allTasks.length === 0 ? (
+          <NoTask />
+        ) : (
+          <>
+            <InfoWrapper>
+              <CompletedTasks
+                allTasks={allTasks}
+                completedTask={completedTask}
+              />
+              <LatestTasks allTasks={latestTask} />
+              <TaskChart
+                taskCount={allTasks.length}
+                completedCount={completedTask.length}
+              />
+            </InfoWrapper>
+            <TaskList
+              allTasks={allTasks}
+              toggleTask={toggleTask}
+              addTask={addTask}
+              deleteTask={deleteTask}
             />
-          </InfoWrapper>
-          <TaskList
-            allTasks={allTasks}
-            toggleTask={toggleTask}
-            addTask={addTask}
-            deleteTask={deleteTask}
-          />
-        </>
-      )}
-    </div>
+          </>
+        )}
+      </Layout>
+    </Container>
   );
 };
 
